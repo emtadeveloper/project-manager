fe3321d4c6dab51efaacaa0450f332dc29511c28
emtadeveloper <47769965+emtadeveloper@users.noreply.github.com>
2023-11-17 13:08:10 +0330

    fix  other route team

--- app/router/router.js
+++ app/router/router.js
@@ -1,12 +1,14 @@
 const {authRoutes} = require("./auth");
 const {userRoutes} = require("./user");
 const {projectRoutes} = require("./project");
+const {teamRoutes} = require("./team");
 
 const router = require("express").Router();
 
 router.use("/auth", authRoutes);
 router.use("/user", userRoutes);
 router.use("/project", projectRoutes);
+router.use("/team", teamRoutes);
 
 module.exports = {
     AllRoutes: router,
---
+++ app/router/team.js
@@ -0,0 +1,25 @@
+const {TeamController} = require("../http/controllers/team.controller");
+const {checkLogin} = require("../http/middlewares/autoLogin");
+const {expressValidatorMapper} = require("../http/middlewares/checkErrors");
+const {mongoIDValidator} = require("../http/validations/mongoID");
+const {createTeamValidator} = require("../http/validations/team");
+
+const router = require("express").Router();
+
+router.post("/create", checkLogin, createTeamValidator(), expressValidatorMapper, TeamController.createTeam);
+
+router.get("/list", checkLogin, TeamController.getAllTeam);
+
+//  هنگام تعریف روت ها آید دار ها در پایین ترین حالت باشه
+
+router.get("/me", checkLogin, TeamController.getMyTeams);
+
+router.get("/:id", checkLogin, mongoIDValidator(), expressValidatorMapper, TeamController.getTeamById);
+
+router.delete("/remove/:id", checkLogin, mongoIDValidator(), expressValidatorMapper, TeamController.removeTeamById);
+
+router.put("/update/:teamID", checkLogin, TeamController.updateTeam);
+
+module.exports = {
+    teamRoutes: router,
+};
